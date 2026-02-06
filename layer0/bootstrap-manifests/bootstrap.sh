7af6212183a3c4fdcd354b8ef480a93dc37eaedd
Bjørnøy, Simon <simon.bjornoy@gmail.com>
2026-02-06 23:36:00 +0100

    add argocd

--- layer0/bootstrap-manifests/bootstrap.sh
+++ layer0/bootstrap-manifests/bootstrap.sh
@@ -3,6 +3,11 @@
 # Install cilium
 helm repo add cilium https://helm.cilium.io/
 helm repo update
-helm install cilium cilium/cilium --namespace kube-system --create-namespace --values cilium/values.yaml
+helm upgrade --install cilium cilium/cilium --namespace kube-system --create-namespace --values cilium/values.yaml
 
-echo "Now we have a working kubernetes api, and we can install the gitops-tool we want to use."
+# Install argo-cd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm upgrade --install argo-cd argo/argo-cd --namespace argocd --create-namespace --values argocd/values.yaml
+
+kubectl apply -f argocd/apps-of-apps.yaml
